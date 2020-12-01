4207373cfad269520c1aa65a7c7bd15ffe15265f
baranyai2 <xpleedke@gmail.com>
2020-12-01 11:38:28 +0100

    Beadandó utolsó simítások

--- XMLFeladatRV4TJX/src/hu/domparse/rv4tjx/DOMModifyRV4TJX.java
+++ XMLFeladatRV4TJX/src/hu/domparse/rv4tjx/DOMModifyRV4TJX.java
@@ -7,6 +7,12 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -101,6 +107,12 @@ public class DOMModifyRV4TJX {
                     }
                     System.out.println();
                 }
+                //Kiíratott értékek átírása egy új XML fájlba
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new File("XMLRV4TJXnew.xml"));
+                transformer.transform(source, result);
             }
 
         }catch(ParserConfigurationException e){
@@ -108,6 +120,10 @@ public class DOMModifyRV4TJX {
         }catch (SAXException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
             e.printStackTrace();
         }
     }
